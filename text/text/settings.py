f4f01ed21e4caafb253c9ef149035aea4430ed14
Justyna Ilczuk <att@atte.ro>
2015-10-28 20:41:42 +0100

    Add static root

--- text/text/settings.py
+++ text/text/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'collectedstatic'
 LOGIN_REDIRECT_URL = '/'
 
 LOGGING = {
